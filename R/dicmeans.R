02cf5aad78c0cd8815bd2dde1e232a66f7371f1f
Akinori Ito <aito@spcom.ecei.tohoku.ac.jp>
2021-11-15 15:30:18 +0900

    Improve the k-means clustering phase

--- R/dicmeans.R
+++ R/dicmeans.R
@@ -17,6 +17,19 @@ renumber <- function(x) {
   y
 }
 
+fairest <- function(clss) {
+  fairness <- rep(0,length(clss))
+  #cat("Fairness:\n")
+  for (i in 1:length(clss)) {
+    fq <- as.numeric(table(clss[[i]]$cluster))
+    #cat("  ",fq,"->")
+    fq <- fq/sum(fq)
+    fairness[i] <- -sum(fq*log(fq))
+    #cat("fairness ",fairness[i],"\n")
+  }
+  which.max(fairness)
+}
+
 #' The dichotomic k-means algorithm.
 #' @param x A matrix or data frame. Each row corresponds to the each data.
 #' @param n Number of clusters.
@@ -24,15 +37,20 @@ renumber <- function(x) {
 #' @param algorithm Gigen to kmeans().
 #' @return The clustering result.
 #' @export
-dicmeans <- function(x,n,iter.max=20,algorithm="Lloyd") {
+dicmeans <- function(x,n,iter.max=20,algorithm="Hartigan-Wong") {
   x.max <- x
   org_ind.max <- 1:nrow(x)
   current_cluster_num <- 2
   current_class <- rep(1,nrow(x))
   while (current_cluster_num <= n) {
-    cls <- stats::kmeans(x.max,2,
-                         iter.max=iter.max,
-                         algorithm=algorithm)
+    clss <- list()
+    for (i in 1:3) {
+      clss[[i]] <- stats::kmeans(x.max,2,
+                           iter.max=iter.max,
+                           algorithm=algorithm)
+      
+    }
+    cls <- clss[[fairest(clss)]]
     current_class[org_ind.max] <- cls$cluster+current_cluster_num-1
     current_class <- renumber(current_class)
     tbl <- base::table(current_class)
